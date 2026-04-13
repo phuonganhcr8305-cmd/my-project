7ea8c901d67f3a55f75cb627f08505603453f2b0
phuonganhcr8305-cmd <phuonganhcr8305@gmail.com>
2026-04-14 03:04:43 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
