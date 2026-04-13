cd541e8317ad390fddfc15046370dd5f92940b71
phuonganhcr8305-cmd <phuonganhcr8305@gmail.com>
2026-04-14 03:01:55 +0700

    Create AboutUs.jsx

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is our company website.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
